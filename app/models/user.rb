05203ff63a6fbfcfa4ed2ea652745337c9447794
emfy0 <pasha.egorov.02.p@ya.ru>
2022-05-13 01:03:28 +0300

    added subscriptions fixex done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :events, dependent: :delete_all
-  has_many :comments
+
+  has_many :events, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 35 }
 
   before_validation :set_name, on: :create
 
+  after_commit :link_subscriptions, on: :create
+
   private
 
   def set_name
     self.name = "Пользователь #{rand(777)}" if self.name.blank?
+  end
+
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+                .update_all(user_id: self.id)
   end
 end
