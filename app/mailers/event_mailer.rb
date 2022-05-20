940da8e94094f5c87bd347b5db9a59fbb860882e
emfy0 <pasha.egorov.02.p@ya.ru>
2022-05-20 23:28:36 +0300

    added comment & subscribers mailers (working)

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -5,13 +5,13 @@ class EventMailer < ApplicationMailer
     @name = subscription.user_name
     @event = event
 
-    mail to: event.user.email, subject: L18t.t('mail.new_subscription', event: event.title)
+    mail to: event.user.email, subject: I18n.t('mail.new_subscription', event: event.title)
   end
 
   def comment(event, comment, email)
     @comment = comment
     @event = event
 
-    mail to: email, subject: L18t.t('mail.new_comment', event: event.title)
+    mail to: email, subject: I18n.t('mail.new_comment', event: event.title)
   end
 end
