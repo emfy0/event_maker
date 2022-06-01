fb7eec3f002eea45f839bee2e8bebbbe58ab102f
emfy <moonmeander47@ya.ru>
2022-06-01 15:25:52 +0300

    comment bug fix

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: Rails.application.credentials.dig(:mailjet, :from)
+  default from: 'no-reply@eventmaker.emfy0.xyz'
   layout "mailer"
 end
