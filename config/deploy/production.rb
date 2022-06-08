614fe900c8e16006f9592418b74143b6820e7de4
pasha <moonmeander47@ya.ru>
2022-06-08 20:55:08 +0300

    changed host

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '62.113.103.13', user: 'kali', roles: %w[app db web resque_worker]
+server '62.113.103.13', user: 'deploy', roles: %w[app db web resque_worker]
 
 set :resque_environment_task, true
 
