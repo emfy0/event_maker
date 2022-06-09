3d029423d1ada150523a84ca61969d5207c9693b
pasha <moonmeander47@ya.ru>
2022-06-10 00:47:17 +0300

    test refactor

--- spec/policies/models/user_spec.rb
+++ spec/policies/models/user_spec.rb
@@ -14,7 +14,6 @@ RSpec.describe User, type: :model do
     context 'when user is not found' do
       let(:user) { User.find_for_vk_oauth(access_token) }
       it 'returns newly created user' do
-
         expect(user).to be_persisted
         expect(user.name).to eq 'UserName'
         expect(user.email).to eq '123@gmail.com'
@@ -33,7 +32,7 @@ RSpec.describe User, type: :model do
     context 'when user is found by provider + provider_id' do
       let!(:existing_user) do
         create(:user, provider: 'vkontakte',
-          provider_id: '123321')
+                      provider_id: '123321')
       end
       let!(:some_other_uer) { create(:user) }
 
@@ -56,7 +55,6 @@ RSpec.describe User, type: :model do
     context 'when user is not found' do
       let(:user) { User.find_for_yandex_oauth(access_token) }
       it 'returns newly created user' do
-
         expect(user).to be_persisted
         expect(user.name).to eq 'UserName'
         expect(user.email).to eq '123@gmail.com'
@@ -75,7 +73,7 @@ RSpec.describe User, type: :model do
     context 'when user is found by provider + provider_id' do
       let!(:existing_user) do
         create(:user, provider: 'yandex',
-          provider_id: '123321')
+                      provider_id: '123321')
       end
       let!(:some_other_uer) { create(:user) }
 
